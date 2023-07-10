8daec9454705879c82211d98696b298172d40ad6
Eduard <erkhusainov17@gmail.com>
2023-07-10 09:36:36 +0200

    fix routing, add webstorm-codestyle-schema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegistrationComponent } from "../pages/registration/registration.component";
-import {LandingComponent} from "../pages/landing/landing.component";
-import {NotFoundComponent} from "../pages/not-found/not-found.component";
+import { RegistrationComponent } from '../pages/registration/registration.component';
+import { LandingComponent } from '../pages/landing/landing.component';
+import { NotFoundComponent } from '../pages/not-found/not-found.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
-  { path: '', component: LandingComponent },
-  { path: 'registration', component: RegistrationComponent },
-  { path: '**', component: NotFoundComponent },
+  {path: '', component: LandingComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from "../pages/landing/landing.component";
-import { SharedModule } from "../shared/shared.module";
-import { ReactiveFormsModule } from "@angular/forms";
-import { LocationStrategy, PathLocationStrategy } from "@angular/common";
+import { LandingComponent } from '../pages/landing/landing.component';
+import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     LandingComponent,
     SharedModule,
     ReactiveFormsModule,
   ],
-
-  providers: [ {provide: LocationStrategy, useClass: PathLocationStrategy} ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
