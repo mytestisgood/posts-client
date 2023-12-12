0dc594221a0d3fdd360293f9d3e343cd8f1aafbd
shoshi <shoshi@smarti.co.il>
2023-12-12 16:24:40 +0200

    navighate

--- src/shared/services/navigation-anchor.service.ts
+++ src/shared/services/navigation-anchor.service.ts
@@ -16,42 +16,52 @@ export class NavigationAnchorService {
   }
 
   public navigateToPageWithAnchor(link: string, anchor?: string): void {
-    const hash: string = window.location.hash.replace('#', '');
-
-    switch (this.router.url) {
-      case link:
-        if (anchor === undefined) {
-          this.router.navigate([this.mainLink]);
-        }
-        break;
-
-      case this.contactLink:
+    // const hash: string = window.location.hash.replace('#', '');
+    switch (anchor) {
+      case 'contact':
         this.router.navigate([link], { fragment: anchor, replaceUrl: true });
-        if (anchor) {
-          this.scrollToElement(anchor as string);
-        }
         break;
-
-      default:
-        if (anchor && hash !== '') {
-          location.hash = '';
-          this.router.events.pipe(
-            filter((e: NavigationEvent) => e instanceof Scroll),
-            takeUntil(this.destroy$),
-          ).subscribe((e: NavigationEvent) => {
-            if (!(!(e instanceof Scroll) || e.routerEvent instanceof NavigationSkipped) &&
-              e.routerEvent.url === '/main') {
-              this.scrollToElement(anchor as string);
-            }
-          });
-        }
+      case 'about':
+      case 'solutions':
+        this.router.navigate([this.mainLink + '#' + anchor]);
+        // this.scrollToElement(anchor as string);
         break;
     }
+    // switch (this.router.url) {
+    //   case link:
+    //     if (anchor === undefined) {
+    //       this.router.navigate([this.mainLink]);
+    //     }
+    //     break;
+    //
+    //   case this.contactLink:
+    //     this.router.navigate([link], { fragment: anchor, replaceUrl: true });
+    //     if (anchor) {
+    //       this.scrollToElement(anchor as string);
+    //     }
+    //     break;
+    //
+    //   default:
+    //     if (anchor && hash !== '') {
+    //       location.hash = '';
+    //       this.router.events.pipe(
+    //         filter((e: NavigationEvent) => e instanceof Scroll),
+    //         takeUntil(this.destroy$),
+    //       ).subscribe((e: NavigationEvent) => {
+    //         if (!(!(e instanceof Scroll) || e.routerEvent instanceof NavigationSkipped) &&
+    //           e.routerEvent.url === '/main') {
+    //           this.scrollToElement(anchor as string);
+    //         }
+    //       });
+    //     }
+    //     break;
+    // }
   }
 
   public scrollToElement(anchor: string): void {
     const element: HTMLElement = document.getElementById(anchor) as HTMLElement;
-    const anchorOffset: 220 | 140 = anchor === 'about' ? 220 : 140;
+    const anchorOffset: 220 | 140 = anchor === 'solutions' ? 220 : 140;
+    alert(anchorOffset);
     const offsetValue: number = element?.offsetTop - anchorOffset;
 
     window.scrollTo({ top: offsetValue, behavior: 'smooth' });
